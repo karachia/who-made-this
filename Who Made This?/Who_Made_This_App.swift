9dfdb9cce593fefc7aa1851c8494446543f580c7
karachia <karachia@usc.edu>
2022-01-08 23:05:07 -0800

    Initial Commit

---
+++ Who Made This?/Who_Made_This_App.swift
@@ -0,0 +1,17 @@
+//
+//  Who_Made_This_App.swift
+//  Who Made This?
+//
+//  Created by Sina Karachiani on 1/8/22.
+//
+
+import SwiftUI
+
+@main
+struct Who_Made_This_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Who Made This?UITests/Who_Made_This_UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Who_Made_This_UITestsLaunchTests.swift
+//  Who Made This?UITests
+//
+//  Created by Sina Karachiani on 1/8/22.
+//
+
+import XCTest
+
+class Who_Made_This_UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
